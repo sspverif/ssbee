sspverif/sspverif

Make SampleName exist

src/parser/package.rs
@@ -888,7 +888,7 @@ pub fn handle_code(
                         tipe.clone(),
                     )
                     ?;
-                    Statement::Sample(ident, None, None, tipe, full_span)
+                    Statement::Sample(ident, None, None, tipe, None, full_span)
                 }
 
                 Rule::assign => {
@@ -926,7 +926,7 @@ pub fn handle_code(
                         Type::Table(Box::new(index.get_type()),  Box::new(tipe.clone())),
                     )
                         ?;
-                    Statement::Sample(ident, Some(index), None, tipe, full_span)
+                    Statement::Sample(ident, Some(index), None, tipe, None, full_span)
                 }
 
                 Rule::table_assign => {

src/transforms/samplify.rs
@@ -129,7 +129,7 @@ pub fn samplify(
                 file_pos,
             )),
 
-            Statement::Sample(id, expr, None, tipe, file_pos) => {
+            Statement::Sample(id, expr, None, tipe, sample_name, file_pos) => {
                 let pos = Position {
                     game_name: game_name.to_string(),
                     inst_name: inst_name.to_string(),
@@ -146,6 +146,7 @@ pub fn samplify(
                     expr,
                     Some(*ctr),
                     tipe.clone(),
+					sample_name,
                     file_pos,
                 ));
                 *ctr += 1;

src/writers/smt/writer.rs
@@ -110,7 +110,7 @@ impl<'a> CompositionSmtWriter<'a> {
                                  game:{game_inst_name}(game_name) pkg:{pkg_inst_name}({pkg_name}) oracle:{oracle_name}", game_inst_name = game_inst_name, pkg_inst_name = pkg_inst_name, pkg_name = pkg_name, oracle_name = oracle_name)
                 }
                 // TODO actually use the type that we sample to know how far to advance the randomness tape
-                Statement::Sample(ident, opt_idx, sample_id, tipe, _) => {
+                Statement::Sample(ident, opt_idx, sample_id, tipe, _, _) => {
                     self.smt_build_sample(oracle_ctx, result, ident, opt_idx, sample_id, tipe)
                 }
                 Statement::Parse(idents, expr, _) => {

src/writers/tex/writer.rs
@@ -315,7 +315,7 @@ impl<'a> BlockWriter<'a> {
                     unreachable!();
                 }
             }
-            Statement::Sample(ident, None, maybecnt, tipe, _) => {
+            Statement::Sample(ident, None, maybecnt, tipe, _, _) => {
                 let cnt = maybecnt.expect("Expected samplified input");
 
                 writeln!(
@@ -327,7 +327,7 @@ impl<'a> BlockWriter<'a> {
                     self.type_to_tex(tipe)
                 )?;
             }
-            Statement::Sample(ident, Some(idxexpr), maybecnt, tipe, _) => {
+            Statement::Sample(ident, Some(idxexpr), maybecnt, tipe, _, _) => {
                 let cnt = maybecnt.expect("Expected samplified input");
 
                 writeln!(

sspverif/ssbee

src/parser/package.rs
@@ -978,7 +978,7 @@ pub fn handle_code(
                         ty.clone(),
                     )
                     ?;
-                    Statement::Sample(ident, None, None, ty, full_span)
+                    Statement::Sample(ident, None, None, ty, None, full_span)
                 }
 
                 Rule::assign => {
@@ -1016,7 +1016,7 @@ pub fn handle_code(
                         Type::Table(Box::new(index.get_type()),  Box::new(ty.clone())),
                     )
                         ?;
-                    Statement::Sample(ident, Some(index), None, ty, full_span)
+                    Statement::Sample(ident, Some(index), None, ty, None, full_span)
                 }
 
                 Rule::table_assign => {

src/transforms/samplify.rs
@@ -129,7 +129,7 @@ pub fn samplify(
                 file_pos,
             )),
 
-            Statement::Sample(id, expr, None, ty, file_pos) => {
+            Statement::Sample(id, expr, None, ty, sample_name, file_pos) => {
                 let pos = Position {
                     game_name: game_name.to_string(),
                     inst_name: inst_name.to_string(),
@@ -146,6 +146,7 @@ pub fn samplify(
                     expr,
                     Some(*ctr),
                     ty.clone(),
+                    sample_name,
                     file_pos,
                 ));
                 *ctr += 1;

src/writers/smt/writer.rs
@@ -112,7 +112,7 @@ impl<'a> CompositionSmtWriter<'a> {
                                  game:{game_inst_name}(game_name) pkg:{pkg_inst_name}({pkg_name}) oracle:{oracle_name}", game_inst_name = game_inst_name, pkg_inst_name = pkg_inst_name, pkg_name = pkg_name, oracle_name = oracle_name)
                 }
                 // TODO actually use the type that we sample to know how far to advance the randomness tape
-                Statement::Sample(ident, opt_idx, sample_id, ty, _) => {
+                Statement::Sample(ident, opt_idx, sample_id, ty, _, _) => {
                     self.smt_build_sample(oracle_ctx, result, ident, opt_idx, sample_id, ty)
                 }
                 Statement::Parse(idents, expr, _) => {

src/writers/tex/writer.rs
@@ -316,7 +316,7 @@ impl<'a> BlockWriter<'a> {
                     unreachable!();
                 }
             }
-            Statement::Sample(ident, None, maybecnt, ty, _) => {
+            Statement::Sample(ident, None, maybecnt, ty, _, _) => {
                 let cnt = maybecnt.expect("Expected samplified input");
 
                 writeln!(
@@ -328,7 +328,7 @@ impl<'a> BlockWriter<'a> {
                     self.type_to_tex(ty)
                 )?;
             }
-            Statement::Sample(ident, Some(idxexpr), maybecnt, ty, _) => {
+            Statement::Sample(ident, Some(idxexpr), maybecnt, ty, _, _) => {
                 let cnt = maybecnt.expect("Expected samplified input");
 
                 writeln!(