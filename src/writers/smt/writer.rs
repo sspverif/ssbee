sspverif/sspverif

Use SampleId when sampling

src/writers/smt/writer.rs
@@ -112,9 +112,16 @@ impl<'a> CompositionSmtWriter<'a> {
                                  game:{game_inst_name}(game_name) pkg:{pkg_inst_name}({pkg_name}) oracle:{oracle_name}", game_inst_name = game_inst_name, pkg_inst_name = pkg_inst_name, pkg_name = pkg_name, oracle_name = oracle_name)
                 }
                 // TODO actually use the type that we sample to know how far to advance the randomness tape
-                Statement::Sample(ident, opt_idx, sample_id, tipe, _, _) => {
-                    self.smt_build_sample(oracle_ctx, result, ident, opt_idx, sample_id, tipe)
-                }
+                Statement::Sample(ident, opt_idx, sample_id, tipe, sample_name, _) => self
+                    .smt_build_sample(
+                        oracle_ctx,
+                        result,
+                        ident,
+                        opt_idx,
+                        sample_id,
+                        sample_name,
+                        tipe,
+                    ),
                 Statement::Parse(idents, expr, _) => {
                     self.smt_build_parse(oracle_ctx, result, idents, expr)
                 }
@@ -638,9 +645,15 @@ impl<'a> CompositionSmtWriter<'a> {
         ident: &Identifier,
         opt_idx: &Option<Expression>,
         sample_id: &Option<usize>,
+        sample_name: &Option<String>,
         tipe: &Type,
     ) -> SmtExpr {
         let sample_id = sample_id.expect("found a None sample_id");
+        let sample_identifier = if let Some(sname) = sample_name {
+            sname
+        } else {
+            &format!("{}", sample_id)
+        };
         let game_inst_ctx = self.context();
 
         let game_inst_name = game_inst_ctx.game_inst_name();
@@ -658,8 +671,15 @@ impl<'a> CompositionSmtWriter<'a> {
             });
 
         let rand_fn_name = names::fn_sample_rand_name(game_inst_name, tipe);
+        let sample_id_struct: SmtExpr = (
+            "sample-id",
+            format!("\"{}\"", oracle_ctx.pkg_inst_ctx().pkg_inst_name()),
+            format!("\"{}\"", oracle_ctx.oracle_name()),
+            format!("\"{}\"", sample_identifier),
+        )
+            .into();
 
-        let rand_val: SmtExpr = (rand_fn_name, format!("{sample_id}"), ctr.clone()).into();
+        let rand_val: SmtExpr = (rand_fn_name, sample_id_struct, ctr.clone()).into();
 
         let new_val = if let Some(idx) = opt_idx {
             ("store", ident.clone(), idx.clone(), rand_val.clone()).into()
@@ -1105,7 +1125,10 @@ impl<'a> CompositionSmtWriter<'a> {
                 (
                     "declare-fun",
                     format!("__sample-rand-{}-{}", game_inst.name, smt_sort.to_string()),
-                    (SmtExpr::Atom("Int".into()), SmtExpr::Atom("Int".into())),
+                    (
+                        SmtExpr::Atom("SampleId".into()),
+                        SmtExpr::Atom("Int".into()),
+                    ),
                     smt_sort,
                 )
                     .into()

sspverif/ssbee

src/writers/smt/writer.rs
@@ -112,9 +112,16 @@ impl<'a> CompositionSmtWriter<'a> {
                                  game:{game_inst_name}(game_name) pkg:{pkg_inst_name}({pkg_name}) oracle:{oracle_name}", game_inst_name = game_inst_name, pkg_inst_name = pkg_inst_name, pkg_name = pkg_name, oracle_name = oracle_name)
                 }
                 // TODO actually use the type that we sample to know how far to advance the randomness tape
-                Statement::Sample(ident, opt_idx, sample_id, ty, _, _) => {
-                    self.smt_build_sample(oracle_ctx, result, ident, opt_idx, sample_id, ty)
-                }
+                Statement::Sample(ident, opt_idx, sample_id, ty, sample_name, _) => self
+                    .smt_build_sample(
+                        oracle_ctx,
+                        result,
+                        ident,
+                        opt_idx,
+                        sample_id,
+                        sample_name,
+                        ty,
+                    ),
                 Statement::Parse(idents, expr, _) => {
                     self.smt_build_parse(oracle_ctx, result, idents, expr)
                 }
@@ -639,8 +646,14 @@ impl<'a> CompositionSmtWriter<'a> {
         opt_idx: &Option<Expression>,
         sample_id: &Option<usize>,
         ty: &Type,
+        sample_name: &Option<String>,
     ) -> SmtExpr {
         let sample_id = sample_id.expect("found a None sample_id");
+        let sample_identifier = if let Some(sname) = sample_name {
+            sname
+        } else {
+            &format!("{}", sample_id)
+        };
         let game_inst_ctx = self.context();
 
         let game_inst_name = game_inst_ctx.game_inst_name();
@@ -657,8 +670,15 @@ impl<'a> CompositionSmtWriter<'a> {
             });
 
         let rand_fn_name = names::fn_sample_rand_name(game_inst_name, ty);
+        let sample_id_struct: SmtExpr = (
+            "sample-id",
+            format!("\"{}\"", oracle_ctx.pkg_inst_ctx().pkg_inst_name()),
+            format!("\"{}\"", oracle_ctx.oracle_name()),
+            format!("\"{}\"", sample_identifier),
+        )
+            .into();
 
-        let rand_val: SmtExpr = (rand_fn_name, format!("{sample_id}"), ctr.clone()).into();
+        let rand_val: SmtExpr = (rand_fn_name, sample_id_struct, ctr.clone()).into();
 
         let new_val = if let Some(idx) = opt_idx {
             ("store", ident.clone(), idx.clone(), rand_val.clone()).into()
@@ -1103,8 +1123,11 @@ impl<'a> CompositionSmtWriter<'a> {
             .map(|smt_sort| {
                 (
                     "declare-fun",
-                    format!("__sample-rand-{}-{}", game_inst.name, smt_sort),
-                    (SmtExpr::Atom("Int".into()), SmtExpr::Atom("Int".into())),
+                    format!("__sample-rand-{}-{}", game_inst.name, smt_sort.to_string()),
+                    (
+                        SmtExpr::Atom("SampleId".into()),
+                        SmtExpr::Atom("Int".into()),
+                    ),
                     smt_sort,
                 )
                     .into()